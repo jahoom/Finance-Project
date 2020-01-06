602080d5af43d6dada6f5109e83eada186182d5b
Jan Macioszczyk <janmac89@gmail.com>
2020-01-06 13:02:39 +0100

    Dodano stopy zwrotow

--- finance-project.py
+++ finance-project.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import datetime
 
+"""
+Pobieranie danych z internetu. Niestety nie pobiera w wymaganym zakresie...
 start = datetime.datetime(2006, 1, 1)
 end = datetime.datetime(2016, 1, 1)
 BAC_df = data.DataReader("BAC", 'stooq', start, end)
@@ -25,3 +27,19 @@ tickers = ['BAC', 'C', 'GS', 'JPM', 'MS', 'WFC']
 bank_stocks = pd.concat([BAC_df, C_df, GS_df, JPM_df, MS_df, WFC_df], axis=1, keys = tickers)
 bank_stocks.columns.names = ['Bank Ticker','Stock Info']
 print(bank_stocks.head())
+"""
+tickers = ['BAC', 'C', 'GS', 'JPM', 'MS', 'WFC']
+
+bank_stocks = pd.read_pickle('all_banks')
+print(bank_stocks.head())
+returns = pd.DataFrame(index = bank_stocks.index)
+
+print("Najwyzsze kursy zamkniecia:\n", bank_stocks.xs(key='Close',axis=1,level='Stock Info').max())
+print("Zwroty: \n", returns.head(10))
+
+print("BAC Return: ", (bank_stocks['BAC']['Close'][1] - bank_stocks['BAC']['Close'][0])/bank_stocks['BAC']['Close'][1])
+
+for ticker in tickers:
+	returns[ticker+" Return"] = bank_stocks[ticker]['Close'].pct_change()
+	
+print(returns.head())
