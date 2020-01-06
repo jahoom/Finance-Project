7f08ffc2a70e0119ca3907d4468d2400da3f398f
Jan Macioszczyk <janmac89@gmail.com>
2020-01-06 10:25:29 +0100

    Dodanie pozostalych bankow

--- finance-project.py
+++ finance-project.py
@@ -6,5 +6,16 @@ import datetime
 start = datetime.datetime(2006, 1, 1)
 end = datetime.datetime(2016, 1, 1)
 BAC_df = data.DataReader("BAC", 'stooq', start, end)
+C_df = data.DataReader("C", 'stooq', start, end)
+GS_df = data.DataReader("GS", 'stooq', start, end)
+JPM_df = data.DataReader("JPM", 'stooq', start, end)
+MS_df = data.DataReader("MS", 'stooq', start, end)
+WFC_df = data.DataReader("WFC", 'stooq', start, end)
 
-print(BAC_df.head())
+
+print("\nBank of America \n", BAC_df.head())
+print("\nCitiGroup \n", C_df.head())
+print("\nGoldman Sachs \n", GS_df.head())
+print("\nJP Morgan \n", JPM_df.head())
+print("\nMorgan Stanley \n", MS_df.head())
+print("\nWells Fargo \n", WFC_df.head())
